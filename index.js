80499cb956bdd54a22fbed722561341d97a5b5d3
annu-adi <rashmi.maurya10@gmail.com>
2025-05-20 19:51:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import noteRouter from './routes/note.js'
 
 const app = express()
 app.use(cors({
-    origin: ["https://noteapp-frontend-4i8v.vercel.app"],
+    origin: ["https://https://noteapp-frontend-ufo7.vercel.app"],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
 }))
