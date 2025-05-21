0ff58852b4fcd7addeb4dd144972efc5efb8bdf1
annu-adi <rashmi.maurya10@gmail.com>
2025-05-21 13:08:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors({
     origin: ["https://noteapp-frontend-ufo7.vercel.app"],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true
-}))
+}));
 app.get('/',(req, res)=>{
 res.json("Hello ji")
 })
